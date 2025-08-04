07a6a0df0aa9e10cabbb93c4c7bb4878d300e2b3
HR0530 <roman053016@gmail.com>
2025-08-04 14:02:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// ページ切り替え処理
 function switchPage(pageName) {
   document.getElementById("subpage").src = `pages/${pageName}/index.html`;
 }
+
+// ログイン状態チェック
+function checkLogin() {
+  if (localStorage.getItem("loggedIn") !== "true") {
+    window.location.href = "login.html";
+  }
+}
+
+// ログアウト処理
+function logout() {
+  localStorage.removeItem("loggedIn");
+  window.location.href = "login.html";
+}
+
