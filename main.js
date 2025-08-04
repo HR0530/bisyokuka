d6e765804b3548ad0447ed2a04a12dfbbe905b69
HR0530 <roman053016@gmail.com>
2025-08-04 14:25:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 // ページ切り替え処理
 function switchPage(pageName) {
-  document.getElementById("subpage").src = `pages/${pageName}/index.html`;
+  const subpage = document.getElementById("subpage");
+  if (subpage) {
+    subpage.src = `pages/${pageName}/index.html`;
+  } else {
+    console.error("iframe 'subpage' が見つかりません");
+  }
 }
 
 // ログイン状態チェック
 function checkLogin() {
-  if (localStorage.getItem("loggedIn") !== "true") {
+  const isLoggedIn = localStorage.getItem("loggedIn");
+  if (isLoggedIn !== "true") {
     window.location.href = "login.html";
   }
 }
@@ -16,3 +22,7 @@ function logout() {
   window.location.href = "login.html";
 }
 
+// 必要ならグローバルに登録
+window.switchPage = switchPage;
+window.checkLogin = checkLogin;
+window.logout = logout;
