848c6858eabad0ef907d1091379eebd3078bfdb7
HR0530 <roman053016@gmail.com>
2025-09-19 10:48:31 +0900

    Update script.js

--- pages/meal/script.js
+++ pages/meal/script.js
@@ -1,6 +1,7 @@
 // ===== pages/meal/script.js（完成版） =====
 // ★必ず “今の ngrok URL” に置き換える
-const API_URL = "https://3299b3961701.ngrok-free.app/api/calc-calorie";
+const API_URL =　"https://https://f8267276b648.ngrok-free.app/api/calc-calorie";
+
 
 // ---- DOM ----
 const els = {
